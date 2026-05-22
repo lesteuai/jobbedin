b75f8860006ac347d2e0b62bf31c9e933092d0e1
bachsofttrick <xuanbach1307@gmail.com>
2026-05-21 20:19:02 -0700

    fix pdf-parse not working in resume route

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   // output: 'export'
-  serverExternalPackages: ['pdf-parse'],
+  serverExternalPackages: ['pdf-parse', 'pdfjs-dist'],
 };
 
 export default nextConfig;
