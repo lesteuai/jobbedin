9bb379a2fe66968bb1edac2cd8b4ceb65110654d
bachsofttrick <xuanbach1307@gmail.com>
2026-07-20 21:52:42 -0700

    adjust drizzle to disable ssl

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -4,7 +4,7 @@ if (!process.env.PGUSER || !process.env.PGPASSWORD || !process.env.PGHOST || !pr
 	throw new Error('Missing required PostgreSQL environment variables: PGUSER, PGPASSWORD, PGHOST, PGPORT, PGDATABASE');
 }
 
-const dbUrl = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}?channel_binding=require&sslmode=require`;
+const dbUrl = `postgresql://${process.env.PGUSER}:${process.env.PGPASSWORD}@${process.env.PGHOST}:${process.env.PGPORT}/${process.env.PGDATABASE}?${process.env.PGSSL === 'true' ? 'channel_binding=require&sslmode=require' : '' }`;
 
 export default defineConfig({
 	schema: './app/lib/db/schema.ts',
